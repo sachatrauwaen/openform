c8fbbd37b3ff1023478d9f989ffbdf4d16ae7d64
Robrecht Siera <robrecht.siera@holoncom.eu>
2016-08-21 00:33:40 +0200

    adjust assembly version to follow the module version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("02.01.03.00")]
-[assembly: AssemblyFileVersion("02.01.03.00")]
+[assembly: AssemblyVersion("03.00.01.00")]
+[assembly: AssemblyFileVersion("03.00.01.00")]
